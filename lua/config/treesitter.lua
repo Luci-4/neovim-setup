28f7d27936a3fbba5605fbc3dc4af300ae3fe63c
wojci <wojciech.kurdybacha@gmail.com>
2024-07-02 00:33:01 +0200

    disable unnecessary treesitter install

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "lua", "cpp"}, -- parsers you want to install
-    sync_install = false,
-    auto_install = true,
+    -- ensure_installed = {"c", "lua", "cpp"}, -- parsers you want to install
+    -- sync_install = false,
+    -- auto_install = true,
     highlight = {
         enable = true, -- false will disable the whole extension
         additional_vim_regex_highlighting = false,
